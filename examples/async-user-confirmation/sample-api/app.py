eb1fdd2a4f617a9bebc5cfd05df844d3e02195b6
iaco <iaco@okta.com>
2025-03-08 16:55:38 +0000

    check scopes for sample-api

--- examples/async-user-confirmation/sample-api/app.py
+++ examples/async-user-confirmation/sample-api/app.py
@@ -3,6 +3,7 @@ import os
 from functools import wraps
 from dotenv import load_dotenv
 from flask import Flask, jsonify, request
+from typing import List, Optional
 from flask_cors import cross_origin
 from jose import jwt
 from six.moves.urllib.request import urlopen
@@ -36,26 +37,30 @@ def get_token_auth_header():
     token = parts[1]
     return token
 
-def requires_auth(f):
-    @wraps(f)
-    def decorated(*args, **kwargs):
-        token = get_token_auth_header()
-        jsonurl = urlopen("https://"+AUTH0_DOMAIN+"/.well-known/jwks.json")
-        jwks = json.loads(jsonurl.read())
-        unverified_header = jwt.get_unverified_header(token)
-        rsa_key = {}
-        for key in jwks["keys"]:
-            if key["kid"] == unverified_header["kid"]:
-                rsa_key = {
-                    "kty": key["kty"],
-                    "kid": key["kid"],
-                    "use": key["use"],
-                    "n": key["n"],
-                    "e": key["e"]
-                }
-        if rsa_key:
+def requires_auth(scopes: Optional[List[str]] = None):
+
+    def decorator(f):
+        @wraps(f)
+        def decorated(*args, **kwargs):
+            token = get_token_auth_header()
+            jsonurl = urlopen("https://"+AUTH0_DOMAIN+"/.well-known/jwks.json")
+            jwks = json.loads(jsonurl.read())
+            unverified_header = jwt.get_unverified_header(token)
+            rsa_key = {}
+            for key in jwks["keys"]:
+                if key["kid"] == unverified_header["kid"]:
+                    rsa_key = {
+                        "kty": key["kty"],
+                        "kid": key["kid"],
+                        "use": key["use"],
+                        "n": key["n"],
+                        "e": key["e"]
+                    }
+            if not rsa_key:
+                raise AuthError({"code": "invalid_header", "description": "unable to find appropriate key"}, 400)
+            
             try:
-                payload = jwt.decode(
+                jwt_payload = jwt.decode(
                     token,
                     rsa_key,
                     algorithms=["RS256"],
@@ -66,16 +71,20 @@ def requires_auth(f):
                 raise AuthError({"code": "token_expired", "description": "token is expired"}, 401)
             except jwt.JWTClaimsError:
                 raise AuthError({"code": "invalid_claims", "description": "incorrect claims, please check the audience and issuer"}, 401)
-            except Exception:
-                raise AuthError({"code": "invalid_header", "description": "Unable to parse authentication token."}, 400)
+            except Exception as e:
+                raise AuthError({"code": "server_error", "description": "unable to verify access token"}, 400)
+            
+            is_authorized = not scopes or all(scope in jwt_payload.get("scope", "").strip().split() for scope in scopes)
+            if not is_authorized:
+                raise AuthError({"code": "insufficient_scope", "description": f"insufficient scope, expected: {" ".join(scopes)}"}, 403)
 
             return f(*args, **kwargs)
-        raise AuthError({"code": "invalid_header", "description": "Unable to find appropriate key"}, 400)
-    return decorated
+        return decorated
+    return decorator
 
 @app.route('/', methods=['POST'])
 @cross_origin(headers=['Content-Type', 'Authorization'])
-@requires_auth
+@requires_auth(scopes=["stock:trade"])
 def stock_purchase():
     # TODO: validate scope
     print("Received request to purchase stock")
