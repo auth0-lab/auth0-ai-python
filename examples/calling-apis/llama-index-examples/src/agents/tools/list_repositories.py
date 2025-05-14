f1a85313105f8eda952590d5fecd327cd78ba68c
Cristian Douce <cristian@okta.com>
2025-05-14 17:03:27 -0300

    feat(federated-connection): add missing method get_access_token_for_connection (#37)

--- examples/calling-apis/llama-index-examples/src/agents/tools/list_repositories.py
+++ examples/calling-apis/llama-index-examples/src/agents/tools/list_repositories.py
@@ -2,19 +2,19 @@ from github import Github
 from github.GithubException import BadCredentialsException
 from llama_index.core.tools import FunctionTool
 
-from auth0_ai_llamaindex.federated_connections import FederatedConnectionError, get_credentials_for_connection
+from auth0_ai_llamaindex.federated_connections import FederatedConnectionError, get_access_token_for_connection
 from src.auth0.auth0_ai import with_github_access
 
 
 def list_repositories_tool_function():
-    credentials = get_credentials_for_connection()
-    if not credentials:
+    access_token = get_access_token_for_connection()
+    if not access_token:
         raise ValueError(
             "Authorization required to access the Federated Connection API")
 
     # GitHub SDK
     try:
-        g = Github(credentials["access_token"])
+        g = Github(access_token)
         user = g.get_user()
         repos = user.get_repos()
         repo_names = [repo.name for repo in repos]
