AdamWozniewski/auth0-ai-python-update

Add Authorization_details

packages/auth0-ai/auth0_ai/credentials.py
@@ -1,4 +1,11 @@
-from typing import TypedDict, Optional
+from typing import TypedDict, Optional, List
+from pydantic import BaseModel
+
+class AuthorizationDetails(BaseModel):
+    type: str
+    class Config:
+        extra = 'allow'
+        allow_mutation = False
 
 class TokenResponse(TypedDict):
     access_token: str
@@ -7,3 +14,4 @@ class TokenResponse(TypedDict):
     token_type: Optional[str]
     id_token: Optional[str]
     refresh_token: Optional[str]
+    authorization_details: Optional[List[AuthorizationDetails]]

auth0-lab/auth0-ai-python

packages/auth0-ai/auth0_ai/credentials.py
@@ -1,5 +1,8 @@
 from typing import TypedDict, Optional
 
+class AuthorizationDetails(TypedDict):
+    type: str
+
 class TokenResponse(TypedDict):
     access_token: str
     expires_in: int
@@ -7,3 +10,4 @@ class TokenResponse(TypedDict):
     token_type: Optional[str]
     id_token: Optional[str]
     refresh_token: Optional[str]
+    authorization_details: Optional[list[AuthorizationDetails]]